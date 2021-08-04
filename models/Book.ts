64ca1fd9ea7c8ee0c5a5b6438990cf061b824616
newt10 <mahavir25.dev@gmail.com>
2021-08-04 15:01:15 -0700

    Add annotations to model for demo

--- models/Book.ts
+++ models/Book.ts
@@ -1,7 +1,18 @@
+/**
+ * @additionalProperties true
+ */
 export interface BookInterface {
   name: string;
   author: string;
   publisher: string;
   keywords?: string[];
+  /**
+   * @pattern ^[0-9a-fA-F]+$
+   */
   barCode: string;
+  /**
+   * @minimum 0
+   * @default 0
+   */
+  price: number;
 }
